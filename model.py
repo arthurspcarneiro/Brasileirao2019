0ab36c40df2ee21c557e5abb489a36950fbd54a6
Arthur Carneiro <arthurspcarneiro@gmail.com>
2019-05-27 11:09:37 -0300

    adding new functions to class

--- model.py
+++ model.py
@@ -8,20 +8,30 @@ import pandas as pd
 from datetime import datetime
 
 
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+
+
 class championship:
     
     def __init__(self,update_time):
         self.update_time = update_time
         self.matches = self.get_matches()
         self.teams = self.get_teams()
-
-    def get_matches(self):
-        url = "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a"
+        self.table = self.classify()
+        
+        
+    
+    def get_matches(self,year=2019):
+        url = "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/"+str(year)
+        #url = "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2018"
         r = requests.get(url)
         data = r.text
         soup = BeautifulSoup(data)
 
-        matches = soup.find("aside").find_all("li")#,attrs={'class':'swiper-wrapper'})
+        matches = soup.find("aside").find_all("li")
         data = []
         for match in matches:
             date = match.find("span",
@@ -48,15 +58,14 @@ class championship:
         df["update_time"] = self.update_time
         df["home_score"] = pd.to_numeric(df["home_score"])
         df["away_score"] = pd.to_numeric(df["away_score"])
-        #df["away_score"] = df["away_score"].astype(np.int64)
-        #df["home_score"] = df["home_score"].astype(np.int64)
         
         return df
     
     def get_teams(self):
         teams_data = self.matches["home_team"].unique()
         teams_data.sort()
-        return pd.DataFrame(teams_data,columns=["short_name_team"])
+        r = np.array(range(len(t)))
+        return pd.DataFrame(r,columns=["index"],index=teams_data)
     
     def classify(self,table=None):
         if table == None:
@@ -80,15 +89,127 @@ class championship:
         won_away.index.name = "team"
         table_home = pd.merge(table_home,won_home,on="team",how="outer")
         table_away = pd.merge(table_away,won_away,on="team",how="outer")
-        stats = pd.merge(table_home,table_away,on="team",how="outer",suffixes=('_home', '_away')).fillna(value=0)
-        stats[["won","gols_dif","gols_for","points"]] = stats.apply(lambda x: pd.Series([x["won_home"]+x["won_away"],#won_counter
+        table = pd.merge(table_home,table_away,on="team",how="outer",suffixes=('_home', '_away')).fillna(value=0)
+        table[["won","gols_dif","gols_for","points"]] = table.apply(lambda x: pd.Series([x["won_home"]+x["won_away"],#won_counter
                                                                                 x["gols_for_home"]+x["gols_for_away"]-
                                                                                 x["gols_against_home"]-x["gols_against_away"], #gol difference
                                                                                 x["gols_for_home"]+x["gols_for_away"], #gols for
                                                                                 x["home_points"]+x["away_points"] #total points        
                                                                                ]) 
                                                                                 , axis=1)
-        stats = stats.sort_values(["points","won","gols_dif","gols_for"],ascending=[0,0,0,0])
-        stats = stats.reset_index()
+        table = table.sort_values(["points","won","gols_dif","gols_for"],ascending=[0,0,0,0])
+        #table = stats.reset_index()
+        #self.table = table
         
-        return stats
+        return table
+    
+    def team_position(self,team):
+        if team is self.teams.values:
+            return cl[cl["team"]==team].index[0]
+        else:
+            print("There is no "+team+ " team!")
+    
+    def team_index(self,team):
+        return self.teams.loc[team][0]
+       
+    def get_parameters(self,fator = 1):
+
+        table = self.matches.dropna()
+        n_teams = len(self.teams)
+
+        ngmarcc = [0.0 for i in range(n_teams)]
+        ngmarcf = [0.0 for i in range(n_teams)]
+        ngsofrc = [0.0 for i in range(n_teams)]
+        ngsofrf = [0.0 for i in range(n_teams)]
+        betac = [1.0 for i in range(n_teams)]
+        betaf = [1.0 for i in range(n_teams)]
+        alfac = [1.0 for i in range(n_teams)]
+        alfaf = [1.0 for i in range(n_teams)]
+
+        beta_ant = [0.0 for i in range(2*n_teams)]
+        beta_atual = betac + betaf
+        alfa_ant = [0.0 for i in range(2*n_teams)]
+        alfa_atual = alfac + alfaf
+        cresc = (float(fator))**(1.0/(2*n_teams-2))
+        w = 1.0
+        tol = 0.000001
+        
+        for i in table.index:
+            w = (cresc)**(int(i/10)+1)
+            t1 = self.team_index(table.loc[i].home_team)
+            t2 = self.team_index(table.loc[i].away_team)
+            ngmarcc[t1] += w*table.loc[i].home_score
+            ngmarcf[t2] += w*table.loc[i].away_score
+            ngsofrc[t1] += w*table.loc[i].away_score
+            ngsofrf[t2] += w*table.loc[i].home_score
+
+        ngsofrc = [x if x!=0 else 1/np.max(ngsofrc) for x in ngsofrc]
+
+        while (np.linalg.norm(np.array(beta_atual)-np.array(beta_ant))> tol):
+         
+            salfac = [0.0 for i in range(n_teams)]
+            salfaf = [0.0 for i in range(n_teams)]
+            sbetac = [0.0 for i in range(n_teams)]
+            sbetaf = [0.0 for i in range(n_teams)]
+
+            for i in range(n_teams):
+                beta_ant[i] = betac[i]
+                beta_ant[n_teams+i] = betaf[i]
+                alfa_ant[i] = alfac[i]
+                alfa_ant[n_teams+i] = alfaf[i]
+
+            for i in table.index:
+                w = (cresc)**(int(i/10)+1)
+                t1 = self.team_index(table.loc[i].home_team)
+                t2 = self.team_index(table.loc[i].away_team)
+
+                sbetaf[t1]  += w/betaf[t2]
+                salfaf[t1]  += w*alfaf[t2]
+                #ngmarcc[t1] += w*table.loc[i].home_score
+
+                sbetac[t2]  += w/betac[t1]
+                salfac[t2]  += w*alfac[t1]
+                #ngmarcf[t2] += w*table.loc[i].away_score
+
+            for i in range(n_teams):
+                alfac[i] = ngmarcc[i]/sbetaf[i]
+                alfaf[i] = ngmarcf[i]/sbetac[i]
+                betac[i] = salfaf[i]/ngsofrc[i]            
+                betaf[i] = salfac[i]/ngsofrf[i]
+
+            #for i in table.index:
+            #    w = (cresc)**(int(i/10)+1)
+            #    t1 = self.team_index(table.loc[i].home_team)
+            #    t2 = self.team_index(table.loc[i].away_team)
+
+            #    salfaf[t1]  += w*alfaf[t2]
+            #    #ngsofrc[t1] += w*table.loc[i].away_score
+
+            #    salfac[t2]  += w*alfac[t1]
+            #    #ngsofrf[t2] += w*table.loc[i].home_score                
+
+
+            #for i in range(n_teams):
+            #    #print(table.loc[i])
+            #    betac[i] = salfaf[i]/ngsofrc[i]            
+            #    betaf[i] = salfac[i]/ngsofrf[i]
+
+            mean = np.mean(betac)
+            for i in range(n_teams):
+                betac[i] /= mean
+
+            mean = np.mean(betaf)
+            for i in range(n_teams):
+                betaf[i] /= mean
+
+            beta_atual = betac + betaf
+            alfa_atual = alfac + alfaf
+
+        p = [alfac, betac, alfaf, betaf]
+        df = pd.DataFrame(p,columns=self.teams.index,index=["alfac","betac","alfaf","betaf"]).T
+        df["home"] = df["alfac"]*df["betac"] 
+        df["away"] = df["alfaf"]*df["betaf"] 
+        df = df.sort_values(["home"],ascending=[0])
+        self.parameters_strength = df
+        
+        return df
